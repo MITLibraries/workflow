9a65ce2103793b6fba6bffe78e1f6b94ad87c623
Mike Graves <mgraves@mit.edu>
2019-10-11 11:51:38 -0400

    Support worker autoscaling
    Adding autoscaling to ECS for the worker service requires a few changes
    to how the cluster is redeployed. Previously, everything was stopped by
    setting the desiredCount to 0 and then started back up by setting the
    desiredCount to 1. With ECS controlling the scaling for the worker
    service it's no longer possible to rely on the desiredCount of the
    worker service remaining at 0 until things are ready to be started
    again.
    
    The change here sets the desiredCount of the scheduler to 0 first, then
    does a --force-new-deployment for the web and worker services, then
    finally does a --force-new-deployment for the scheduler. While
    redeploying becomes more complicated, there's no need anymore to track
    the current worker count. We can just set it to 1 and let ECS scale as
    needed.
    
    I've also added stop-scheduler and start-scheduler commands which can be
    used in conjunction with deploying a new service definition with
    Terraform.

--- tests/test_cluster.py
+++ tests/test_cluster.py
@@ -5,31 +5,50 @@ from manager.cluster import Cluster
 
 
 def test_run_task_returns_taskarn(cluster):
-    c = Cluster('airflow-test', 'scheduler', 'worker', 'web')
+    ecs = boto3.client('ecs')
+    c = Cluster('airflow-test', 'scheduler', 'worker', 'web', ecs)
     assert c.run_task({'command': ['run_things']})\
         .startswith('arn:aws:ecs:us-east-1:012345678910:task/')
 
 
 def test_stop_sets_desired_count_to_zero(cluster):
     ecs = boto3.client('ecs')
-    Cluster(*cluster).stop()
+    Cluster(*cluster, ecs).stop()
     res = ecs.describe_services(cluster=cluster.name, services=cluster[1:])
-    for service in res['services']:
-        assert service['desiredCount'] == 0
+    assert jmespath.search('services[].desiredCount', res) == [0, 0, 0]
 
 
-def test_start_sets_resets_desired_count(cluster):
+def test_stop_stops_specified_service(cluster):
     ecs = boto3.client('ecs')
-    c = Cluster(*cluster)
+    c = Cluster(*cluster, ecs)
+    c.stop(services=[c.scheduler])
+    res = ecs.describe_services(cluster=cluster.name, services=cluster[1:])
+    assert jmespath.search(
+        f"services[?serviceName=='{cluster.scheduler}'].desiredCount|[0]",
+        res) == 0
+    assert jmespath.search(
+        f"services[?serviceName=='{cluster.web}'].desiredCount|[0]",
+        res) == 1
+
+
+def test_start_sets_desired_count(cluster):
+    ecs = boto3.client('ecs')
+    c = Cluster(*cluster, ecs)
     c.stop()
     c.start()
     res = ecs.describe_services(cluster=cluster.name, services=cluster[1:])
-    assert jmespath.search(
-        f"services[?serviceName=='{cluster.worker}'].desiredCount|[0]",
-        res) == 3
+    assert jmespath.search('services[].desiredCount', res) == [1, 1, 1]
+
+
+def test_start_starts_specified_service(cluster):
+    ecs = boto3.client('ecs')
+    c = Cluster(*cluster, ecs)
+    c.stop()
+    c.start(services=[c.scheduler])
+    res = ecs.describe_services(cluster=cluster.name, services=cluster[1:])
     assert jmespath.search(
         f"services[?serviceName=='{cluster.scheduler}'].desiredCount|[0]",
         res) == 1
     assert jmespath.search(
         f"services[?serviceName=='{cluster.web}'].desiredCount|[0]",
-        res) == 1
+        res) == 0
